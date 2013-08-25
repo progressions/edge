4891d2cc8eaefe14b5ccf3156bb185efaa97994c
Isaac Priestley <progressions@gmail.com>
2013-08-25 18:25:32 -0500

    First commit for Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Edge::Application.config.secret_key_base = 'b07914eff3cc6124d4028ba7c5d5eba3a246ed1bca9477ecb1ccab304b6c6ec20e17b4028252db6cb38dddd39059cbfc349bd2a8916a2f7f2ce2bfe3e83a4378'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Edge::Application.config.session_store :cookie_store, key: '_edge_session'
