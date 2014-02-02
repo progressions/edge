5f21ff5642088fdab03d7a6cf7abb6a388e09d87
Isaac Priestley <progressions@gmail.com>
2014-02-02 17:33:29 -0600

    from xml

--- app/models/character.rb
+++ app/models/character.rb
@@ -18,8 +18,16 @@ class Character < ActiveRecord::Base
     hash = Hash.from_xml(xml)
     character_params = {}
 
-    character_params[:name] = hash["Character"]["Description"]["CharName"]
-    character_params[:player_name] = hash["Character"]["Description"]["PlayerName"]
+    description_hash = hash["Character"]["Description"]
+    character_params[:name] = description_hash["CharName"]
+    character_params[:player_name] = description_hash["PlayerName"]
+    character_params[:gender] = description_hash["Gender"]
+    character_params[:age] = description_hash["Age"]
+    character_params[:height] = description_hash["Height"]
+    character_params[:build] = description_hash["Build"]
+    character_params[:hair] = description_hash["Hair"]
+    character_params[:eyes] = description_hash["Eyes"]
+    character_params[:notable_features] = description_hash["OtherFeatures"]
 
     create(character_params)
   end
