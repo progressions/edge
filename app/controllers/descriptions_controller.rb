b30f4006431f3072c311c86048a37fdfaa5fadbd
Isaac Priestley <progressions@gmail.com>
2014-02-02 19:32:15 -0600

    update description

--- app/controllers/descriptions_controller.rb
+++ app/controllers/descriptions_controller.rb
@@ -5,7 +5,20 @@ class DescriptionsController < ApplicationController
     @character = current_user.characters.find(params[:id])
   end
 
-  def create
+  def update
     @character = current_user.characters.find(params[:id])
+
+
+    if @character.update_attributes(character_params)
+      flash[:notice] = "Your character has been updated."
+    else
+      flash[:notice] = "There was a problem updating your character."
+    end
+
+    redirect_to new_description_url(@character)
+  end
+
+  def character_params
+    params.require(:character).permit(:name, :player_name, :gender, :age, :height, :build, :hair, :eyes, :notable_features)
   end
 end
