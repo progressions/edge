5cd159a5ebd04043e570478d69cb2bf411961c18
Isaac Priestley <progressions@gmail.com>
2014-02-03 13:01:26 -0600

    init rake tasks

--- config/initializers/social_classes.rb
+++
@@ -1,8 +0,0 @@
-begin
-  SOCIAL_CLASSES = HashWithIndifferentAccess.new(YAML.load(File.read(Rails.root.join("config", "social_classes.yml"))))
-
-  SOCIAL_CLASSES.each do |key, values|
-    social_class = SocialClass.where(name: values[:name]).first || SocialClass.create(values)
-  end
-rescue StandardError => e
-end
