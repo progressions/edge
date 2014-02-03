8d1213baa70ace0552c161c4161e40964b8acb7a
Isaac Priestley <progressions@gmail.com>
2014-02-03 11:10:09 -0600

    initialize

---
+++ config/initializers/social_classes.rb
@@ -0,0 +1,5 @@
+SOCIAL_CLASSES = HashWithIndifferentAccess.new(YAML.load(File.read(Rails.root.join("config", "social_classes.yml"))))
+
+SOCIAL_CLASSES.each do |key, values|
+  social_class = SocialClass.where(name: values[:name]).first || SocialClass.create(values)
+end
