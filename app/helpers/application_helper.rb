67312a74450eadce0938b14b693b937e6dc69062
Isaac Priestley <progressions@gmail.com>
2014-02-03 14:15:59 -0600

    Format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def identifier(string)
     string.to_s.underscore.gsub(/[^A-Za-z0-9]/, "")
   end
+
+  def format_description(description)
+    description.gsub("[H4]", "<h4>").gsub("[h4]", "</h4>").html_safe
+  end
 end
