3af1122c57fdd7c194bab53a44c93c5db79437e7
Isaac Priestley <progressions@gmail.com>
2014-02-02 23:06:44 -0600

    backgrounds

--- app/controllers/backgrounds_controller.rb
+++ app/controllers/backgrounds_controller.rb
@@ -2,12 +2,24 @@ class BackgroundsController < ApplicationController
   layout 'characters'
 
   def new
+    @social_classes = ["Down and Out", "Middle Class Struggles", "High and Mighty", "The Outsider"]
+    @backgrounds = ["Opportunity Knocks", "Higher Calling", "Enemies and Antagonists", "Failure of Character", "Wrong Place, Wrong Time", "Academic", "Criminal", "Entrepreneur", "Ex-Imperial Operator", "Fallen Noble", "Wanderer"]
+    @character = current_user.characters.find(params[:id])
   end
 
   def update
+    @character = current_user.characters.find(params[:id])
+
+    if @character.update_attributes(character_params)
+      flash[:notice] = "Your character has been updated."
+    else
+      flash[:notice] = "There was a problem updating your character."
+    end
+
+    redirect_to new_background_url(@character)
   end
 
   def character_params
-    params.require(:character).permit(:social_class, :background_hook, :story)
+    params.require(:character).permit(:social_class, :background, :story)
   end
 end
