78cb5531f223e96b16678603473c2febb881cc33
Isaac Priestley <progressions@gmail.com>
2014-02-06 17:08:00 -0600

    working

--- lib/loader.rb
+++ lib/loader.rb
@@ -1,4 +1,34 @@
 class Loader
+  def self.load_single(values)
+    key = values["Key"]
+    class_values = {}
+
+    values.each do |k, v|
+      next unless v.present?
+
+      k = k.underscore
+      if k == "type"
+        k = "#{klass.name.underscore}_type"
+      end
+
+      if k == "categories"
+        k = "categories_from_xml"
+        v = v["Category"]
+      end
+
+      class_values[k] = v
+    end
+
+    class_values.select! do |k,v|
+      klass.new.respond_to?("#{k}=".to_sym)
+    end
+
+    record = klass.where(key: key).first || klass.new
+    record.update_attributes(class_values)
+
+    puts "Loaded record: #{record.inspect}"
+  end
+
   def self.load_from_xml(name, options={})
     options ||= {}
     begin
@@ -15,33 +45,7 @@ class Loader
       end
 
       @hash[collection_name][member_name].each do |values|
-        key = values["Key"]
-        class_values = {}
-
-        values.each do |k, v|
-          next unless v.present?
-
-          k = k.underscore
-          if k == "type"
-            k = "#{klass.name.underscore}_type"
-          end
-
-          if k == "categories"
-            k = "categories_from_xml"
-            v = v["Category"]
-          end
-
-          class_values[k] = v
-        end
-
-        class_values.select! do |k,v|
-          klass.new.respond_to?("#{k}=".to_sym)
-        end
-
-        record = klass.where(key: key).first || klass.new
-        record.update_attributes(class_values)
-
-        puts "Loaded record: #{record.inspect}"
+        Loader.load_single(values)
       end
     end
   end
