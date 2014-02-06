eb2bed147cc5ba2b5603ac856c37a4f31de54623
Isaac Priestley <progressions@gmail.com>
2014-02-06 16:02:59 -0600

    annotate

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id                 :integer          not null, primary key
+#  name               :string(255)
+#  categorizable_id   :integer
+#  categorizable_type :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
