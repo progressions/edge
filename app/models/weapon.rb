120379789ed54c60f690760bae7ba500fcfe1642
Isaac Priestley <progressions@gmail.com>
2014-02-05 22:51:19 -0600

    index

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+#
+# Table name: weapons
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  key         :string(255)
+#  description :text
+#  skill_key   :string(255)
+#  damage      :integer
+#  crit        :integer
+#  range       :string(255)
+#  encumbrance :integer
+#  hp          :integer
+#  price       :integer
+#  rarity      :integer
+#  weapon_type :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Weapon < ActiveRecord::Base
 end
--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: weapons
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  key         :string(255)
+#  description :text
+#  skill_key   :string(255)
+#  damage      :integer
+#  crit        :integer
+#  range       :string(255)
+#  encumbrance :integer
+#  hp          :integer
+#  price       :integer
+#  rarity      :integer
+#  weapon_type :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 require 'spec_helper'
 
 describe Weapon do
