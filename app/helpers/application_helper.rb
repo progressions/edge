b088b8908b584f6bb2c2422a6b151eb80c3a7966
Isaac Priestley <progressions@gmail.com>
2014-02-07 16:39:09 -0600

    some kinda encoding

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
+# encoding: utf-8
 module ApplicationHelper
   def identifier(string)
     string.to_s.underscore.gsub(/[^A-Za-z0-9]/, "")
   end
 
   def format_description(description)
+    description = description.force_encoding("iso-8859-1").encode("utf-8")
     description.
+      gsub("\n", "").
+      gsub('\"', '"').
       gsub("[H4]", "<h4>").
       gsub("[h4]", "</h4>").
       gsub("[P]", "<p>").
@@ -14,5 +18,7 @@ module ApplicationHelper
       gsub("[I]", "<em>").
       gsub("[i]", "</em>").
       html_safe
+  rescue StandardError => e
+    return e
   end
 end
