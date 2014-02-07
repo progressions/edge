435c6e89fc60447b3ec7ade95b1c3b34e4bf0056
Isaac Priestley <progressions@gmail.com>
2014-02-07 15:21:44 -0600

    starting

--- app/models/species.rb
+++ app/models/species.rb
@@ -28,10 +28,22 @@ class Species < ActiveRecord::Base
   end
 
   def starting_chars=(values)
+    attributes_from_xml(values)
+  end
+
+  def starting_attrs=(values)
+    attributes_from_xml(values)
+  end
+
+  def attributes_from_xml(values)
     values.map do |k,v|
       if self.respond_to?("#{k.underscore}=".to_sym)
         self.update_attribute(k.underscore, v)
       end
     end
+  end
+
+  def experience=(value)
+    self.starting_xp = value
   end
 end
