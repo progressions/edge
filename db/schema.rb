105482756da4ed39361dfb047ee8524dd0800c27
Isaac Priestley <progressions@gmail.com>
2014-02-08 12:12:41 -0600

    options

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140208180906) do
+ActiveRecord::Schema.define(version: 20140208181014) do
 
   create_table "backgrounds", force: true do |t|
     t.string   "name"
