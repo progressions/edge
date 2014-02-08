1693ef711365c4cb94a1b3932ecfbb9eaa5d505f
Isaac Priestley <progressions@gmail.com>
2014-02-07 19:15:41 -0600

    waait

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140208005429) do
+ActiveRecord::Schema.define(version: 20140208005258) do
 
   create_table "backgrounds", force: true do |t|
     t.string   "name"
@@ -49,14 +49,6 @@ ActiveRecord::Schema.define(version: 20140208005429) do
   create_table "categorizables", force: true do |t|
     t.integer  "category_id"
     t.integer  "parent_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "character_career_skills", force: true do |t|
-    t.integer  "character_id"
-    t.integer  "skill_id"
-    t.string   "source"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
